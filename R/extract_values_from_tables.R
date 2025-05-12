eb49201ac350830af025f8d569a6ef4790c639f3
SamuelGonzalez0204 <154916568+SamuelGonzalez0204@users.noreply.github.com>
2025-05-12 12:01:10 +0200

    Update extract_values_from_tables.R

--- R/extract_values_from_tables.R
+++ R/extract_values_from_tables.R
@@ -14,9 +14,10 @@ narrow_text <- function(start_text, start_text2 = "   Variaciones del n\u00famer
   start_indices2 <- grep(start_text2, lines_total)
   limit_index <- grep(text_limit, lines_total)[1]
   limit_index2 <- grep(text_limit2, lines_total)
+
   if (length(start_indices2) != 0 || length(start_indices) > 1) {
     if (length(start_indices2) != 0 && length(start_indices) != 0) {
-      extracted_lines <- lines_total[(start_indices + 1):(start_indices2 - 1)]
+      extracted_lines <- lines_total[(start_indices + 1):(limit_index - 1)]
     } else if (length(limit_index2) != 0 && length(start_indices) != 0) {
       extracted_lines <- lines_total[(start_indices + 1):(limit_index2[1] - 1)]
     } else if (length(start_indices2) != 0) {
@@ -26,12 +27,11 @@ narrow_text <- function(start_text, start_text2 = "   Variaciones del n\u00famer
     }
   } else if (length(start_indices2) == 0 && length(start_indices) == 0) {
     extracted_lines <- c(extracted_lines, "No biomarkers")
-  } else if (!is.na(limit_index[1]) ){
+  } else if (length(limit_index) != 0){
     extracted_lines <- lines_total[(start_indices + 1):(limit_index - 1)]
   }
   return(extracted_lines)
 }
-
 
 #' Extract values from tables within text
 #'
@@ -66,7 +66,7 @@ narrow_text <- function(start_text, start_text2 = "   Variaciones del n\u00famer
 #' codifications <- TableValues[[4]]
 #' changes <- TableValues[[5]]
 #'
-#' @import stringr
+#' @importFrom stringr str_match
 #' @export
 extract_values_from_tables <- function(lines, mutations,
                                        genes_mutated = list(), pathogenicity = list(), frequencies = list(),
@@ -76,31 +76,44 @@ extract_values_from_tables <- function(lines, mutations,
                                        end = "Genes analizados",
                                        end2 ="Comentarios adicionales sobre las variantes") {
   lines <- narrow_text(start, start2, lines, end, end2)
-  positions <- freq_list <- pdf_mutations <- patho_list <- cod_list <- change_list <- c()
+  positions <- mut_pats <- freq_list <- pdf_mutations <- patho_list <- cod_list <- change_list <- c()
   split_lines <- strsplit(lines, "\\s+")
 
-  frequency_pattern <- "\\d{2}\\.\\d{2}\\%"
+  frequency_pattern <- "\\d{1,3}\\.\\d{2}\\%"
   codification_pattern <- "c\\.[0-9]+[A-Za-z>_]+"
+  change_pattern <- "p\\.\\(.*?\\)"
 
   pos <- 0
+  n_mut <- 0
   for (line in split_lines) {
     pos <- pos + 1
     if (line[1] %in% mutations) {
       positions <- c(positions, pos)
+      n_mut <- n_mut+1
       pdf_mutations <- c(pdf_mutations, line[1])
       for (item in strsplit(line, " ")) {
         freq_match <- stringr::str_match(item, frequency_pattern)
         cod_match <- stringr::str_match(item, codification_pattern)
-        change_match <- stringr::str_match(item, "p\\.\\(.*?\\)")
+        change_match <- stringr::str_match(item, change_pattern)
 
         if (!is.na(freq_match)) {
           freq_list <- c(freq_list, freq_match[1])
-        } else if (!is.na(cod_match)) {
+        }
+        if (!is.na(cod_match)) {
           cod_list <- c(cod_list, cod_match[1])
         }
         if (!is.na(change_match)) {
           change_list <- c(change_list, change_match[1])
         }
+      }
+      if (n_mut != length(freq_list)){
+        freq_list <- c(freq_list, "None")
+      }
+      if (n_mut != length(cod_list)){
+        cod_list <- c(cod_list, "None")
+      }
+      if (n_mut != length(change_list)) {
+        change_list <- c(change_list, "None")
       }
     }
   }
